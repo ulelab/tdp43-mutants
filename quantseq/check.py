74502fcfb796117bb468c994e46dcffbf1bf0b26
Marc Jones <marc.jones@crick.ac.uk>
2022-06-22 15:05:05 +0100

    Updated check.py

--- quantseq/check.py
+++ quantseq/check.py
@@ -35,14 +35,17 @@ def get_similar_file_name(str_in, list_in):
     ]
     return(sorted(scores)[-1][1])
 
-def print_comparisons(snakemake_files, nf_quantseq_files):
+def print_comparisons(snakemake_files, nf_quantseq_files, presort_lines=False):
     for snakemake_file_name in snakemake_files:
         if snakemake_file_name.endswith(".gz"):
             in_f = gzip.open(snakemake_file_name, "rt")
         else:
             in_f = open(snakemake_file_name)
         try:
-            snakemake_hash = hashlib.md5(in_f.read().encode()).hexdigest()
+            in_f_str = in_f.read()
+            if presort_lines:
+                in_f_str = "\n".join(sorted(in_f_str.split("\n")))
+            snakemake_hash = hashlib.md5(in_f_str.encode()).hexdigest()
         except UnicodeDecodeError:
             sys.exit("Unicode error reading {}".format(snakemake_file_name))
         in_f.close()
@@ -55,7 +58,10 @@ def print_comparisons(snakemake_files, nf_quantseq_files):
         else:
             in_f = open(nf_quantseq_file_name)
         try:
-            nf_quantseq_hash = hashlib.md5(in_f.read().encode()).hexdigest()
+            in_f_str = in_f.read()
+            if presort_lines:
+                in_f_str = "\n".join(sorted(in_f_str.split("\n")))
+            nf_quantseq_hash = hashlib.md5(in_f_str.encode()).hexdigest()
         except UnicodeDecodeError:
             sys.exit("Unicode error reading {}".format(nf_quantseq_file_name))
         in_f.close()
@@ -78,12 +84,18 @@ def print_comparisons(snakemake_files, nf_quantseq_files):
 snakemake_results = "/camp/home/jonesm5/home/users/jonesm5/projects/tdp43-mutants/quantseq/results/"
 nf_quantseq_results = "/camp/home/jonesm5/home/users/jonesm5/projects/nf-quantseq/results/"
 
+### cutadapt directory
+print("=== {} ===".format("cutadapt"))
+
 # Cutadapt
 print("--- {} ---".format("cutadapt"))
 print_comparisons(
     glob.glob(snakemake_results + "cutadapt/*"),
     glob.glob(nf_quantseq_results + "cutadapt/*")
 )
+
+### polya directory
+print("=== {} ===".format("polya"))
 
 # Cutadapt PolyA trimmed
 print("--- {} ---".format("cutadapt polyA trimmed"))
@@ -100,6 +112,9 @@ print_comparisons(
     glob.glob(nf_quantseq_results + "polyaclusters/*bedgraph")
 )
 
+### mergedclusters directory
+print("=== {} ===".format("mergedclusters"))
+
 # Remove random priming
 
 print("--- {} ---".format("Remove random priming"))
@@ -108,12 +123,40 @@ print_comparisons(
     glob.glob(nf_quantseq_results + "polyaclusters/merged_polya.bed")
 )
 print_comparisons(
+    glob.glob(snakemake_results + "mergedclusters/polyAclusters.bedgraph"),
+    glob.glob(nf_quantseq_results + "polyaclusters/merged_polya.bedgraph")
+)
+print_comparisons(
     glob.glob(snakemake_results + "mergedclusters/polyAclusters.unique.bed"),
     glob.glob(nf_quantseq_results + "polyaclusters/merged_polya.unique.bed")
 )
 print_comparisons(
+    glob.glob(snakemake_results + "mergedclusters/polyAclusters.unique.bedgraph"),
+    glob.glob(nf_quantseq_results + "polyaclusters/merged_polya.unique.bedgraph")
+)
+print_comparisons(
     glob.glob(snakemake_results + "mergedclusters/polyAclusters.unique.annotated.bed"),
     glob.glob(nf_quantseq_results + "polyaclusters/merged_polya.unique.annotated.bed")
+)
+
+### mapped directory
+print("=== {} ===".format("mapped"))
+
+print_comparisons(
+    glob.glob(snakemake_results + "mapped/*fastq.gz"),
+    glob.glob(nf_quantseq_results + "mapped/*fastq.gz")
+)
+
+### counts directory
+print("=== {} ===".format("counts"))
+
+# bamtobed
+
+print("--- {} ---".format("bamtobed"))
+print_comparisons(
+    glob.glob(snakemake_results + "counts/*Aligned.sortedByCoord.out.bed"),
+    glob.glob(nf_quantseq_results + "bedtools/*polya_trimmed.bed"),
+    presort_lines=True
 )
 
 # Count tables
@@ -122,4 +165,12 @@ print("--- {} ---".format("Count tables"))
 print_comparisons(
     glob.glob(snakemake_results + "counts/*bed.gz"),
     glob.glob(nf_quantseq_results + "bedtools/*window_merged_polya.bed")
+)
+
+# Bedgraphs
+
+print("--- {} ---".format("bedgraph"))
+print_comparisons(
+    glob.glob(snakemake_results + "counts/*bedgraph.gz"),
+    glob.glob(nf_quantseq_results + "bedgraphconvert/*")
 )
